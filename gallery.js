761e33da023c65b34701e1586d17265815645850
siersbear <jmsdraws@gmail.com>
2026-02-04 08:17:57 -0500

    update1

---
+++ gallery.js
@@ -0,0 +1,19 @@
+const links = document.querySelectorAll("#galnav a");
+const sections = document.querySelectorAll("#gallery > div");
+
+links.forEach(link => {
+    link.addEventListener("click", e => {
+        e.preventDefault(); // stops jump scrolling
+
+        const targetID = link.getAttribute("href").replace("#", "");
+        const target = document.getElementById(targetID);
+
+        // hide everything
+        sections.forEach(sec => {
+            sec.style.display = "none";
+        });
+
+        // show selected section
+        target.style.display = "block";
+    });
+});
